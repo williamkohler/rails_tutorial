db99013d6bb4dcd7e014defdbdea9a7f7ef92c19
Bill Kohler <bill@thekurlandagency.com>
2017-08-14 15:14:51 -0400

    fixed merge

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,5 @@ Rails.application.routes.draw do
   get  'static_pages/home'
   get  'static_pages/help'
   get  'static_pages/about'
-<<<<<<< HEAD
-  get  'static_pages/contact'
-
-  root 'static_pages#home'
-=======
->>>>>>> static-pages
+  get 'static_pages/contact'
 end
