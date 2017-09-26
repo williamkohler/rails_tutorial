fc3c4d164ceaa784c2cb825080a701b69cb1c6d0
Bill Kohler <bill@thekurlandagency.com>
2017-09-26 12:05:59 -0400

    @12.2 Password reset emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
-  # phased out for a method refrence ':downcast_email'
-  # before_save { self.email = email.downcase }
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\- ]+)*\.[a-z]+\z/i
@@ -56,6 +54,18 @@ end
   # Sends an activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+# Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   private
