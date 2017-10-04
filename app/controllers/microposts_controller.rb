77e2a21c47d8d21c7bfb223d7494349c1a9a5fb3
Bill Kohler <bill@thekurlandagency.com>
2017-10-04 17:23:49 -0400

    @ 13.4 Micropost images

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,22 +1,36 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:sucess] = "Micropost created!"
+      flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    # request.referrer means previous url
+    # redirect_to request.referrer || root_url
+
+    # new for rails 5:
+    redirect_back(fallback_location: root_url)
   end
 
   private
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
