60ed4a93ac444473920887801172bed09c9b5a94
Bill Kohler <bill@thekurlandagency.com>
2017-10-05 11:49:41 -0400

    @ 13.4.2 Image validation

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -11,6 +11,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     get root_path
     # assert_select 'div.pagination'
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     # Invalid submission
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: " "} }
@@ -18,9 +19,12 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # Valid submission
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: {micropost: { content: content} }
+      post microposts_path, params: {micropost: { content: content,
+                                                  picture: picture } }
     end
+    assert assigns(:micropost).picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
