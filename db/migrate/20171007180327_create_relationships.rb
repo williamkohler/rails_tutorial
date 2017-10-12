be04932ac066bc4295024514ecd76682fb36c8c6
Bill Kohler <bkohler4@gmail.com>
2017-10-12 12:13:51 -0400

    @14.1.5 Followers

---
+++ db/migrate/20171007180327_create_relationships.rb
@@ -0,0 +1,14 @@
+class CreateRelationships < ActiveRecord::Migration[5.1]
+  def change
+    create_table :relationships do |t|
+      t.integer :follower_id
+      t.integer :followed_id
+
+      t.timestamps
+    end
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    # Makes sure a user can't follow another user more than once.
+    add_index :relationships, [:follower_id, :followed_id], unique: true
+  end
+end
