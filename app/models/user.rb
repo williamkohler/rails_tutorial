9ab5a2b84156ea68a67ef07bef9319b22807c22b
Bill Kohler <bill@thekurlandagency.com>
2017-10-16 12:00:32 -0400

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -84,9 +84,12 @@ end
 
   # Returns a user's status feed.
   def feed
-    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    following_ids = "SELECT followed_id from relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
-
+  
   # Follows a user.
   def follow(other_user)
     following << other_user
